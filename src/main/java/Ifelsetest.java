beb2067981bd73f9ea13c5a4917747c72e392ec3
[ahmedanayath2@gmail.com] <anayathahmed@Anayaths-MacBook-Pro.local>
2021-02-23 21:20:41 -0500

    HW

---
+++ src/main/java/Ifelsetest.java
@@ -0,0 +1,12 @@
+public class Ifelsetest {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+            if (i < 10) {
+                System.out.println("You are broke");
+            } else {
+                System.out.println("You are rich");
+            }
+        }
+    }
+}
---
+++ src/main/java/WhileTest.java
@@ -0,0 +1,10 @@
+public class WhileTest {
+    public static void main(String[] args) {
+        boolean tired = true;
+
+        if(tired) System.out.println("Go to sleep");
+        else{ System.out.println("Go to gym"); }
+
+
+    }
+}
